3c22d3fe302597dad094925f412685640e8cf741
x78701542000 <x78701542000@livemail.tw>
2019-10-31 09:18:10 +0800

    忘了

--- 天明重開器/ByTCPcWD/Form1.Designer.cs
+++ 天明重開器/ByTCPcWD/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // timer1
@@ -37,21 +41,59 @@
             this.timer1.Interval = 5000;
             this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 426);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(296, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(67, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- 天明重開器/ByTCPcWD/Form1.cs
+++ 天明重開器/ByTCPcWD/Form1.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Windows.Forms;
-using System.Net;
-using System.Net.Sockets;
+
 
 namespace ByTCPcWD
 {
     public partial class Form1 : Form
     {
+        int countdown = 30;
+        const int countdownC = 30;
+
+
+
         public Form1()
         {
             InitializeComponent();
@@ -20,23 +20,154 @@ namespace ByTCPcWD
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             timer1.Enabled = true;
+
 
         }
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
+            timer1.Stop();
+            try
+            {
+                TryConnect();
+
+            }
+            catch (SocketException Sockete)
+            {
+                textBox1.AppendText("Tcp Error" + Environment.NewLine);
+                //throw;
+            }
+
+        }
+
+        private void TryConnect()
+        {
+            textBox1.AppendText("Tcp try" + Environment.NewLine);
+            //TcpClient tcpClient = new TcpClient("192.168.100.9", 4001);
+
+
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.BeginConnect("192.168.100.4", 4001, new AsyncCallback(ConnectED), tcpClient);
+        }
+
+        private void ConnectED(IAsyncResult ar)
+        {
+            //throw new NotImplementedException();
+
+            TcpClient Tcp_C = (TcpClient)ar.AsyncState;
+
+            WriteTextSafe("Tcp Connected = " + Tcp_C.Connected + Environment.NewLine, textBox1);
+
+            if (Tcp_C.Connected)
+            {
+                Tcp_C.Close();
+
+                timer1.Start();
+            }
+            else
+            {
+                //timer2.Enabled = true;
+                timer2.Interval = 1000;
+                timer2.Enabled = true;
+                //Rest();
+
+                WriteTextSafe("Restart DVR By test !!!" + Environment.NewLine, textBox1);
+
+            }
+
+
+        }
+
+        private delegate void SafeCallDelegate(string text, TextBox TB);
+        private void WriteTextSafe(string text, TextBox TB)
+        {
+            if (textBox1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(WriteTextSafe);
+                TB.Invoke(d, new object[] { text, TB });
+            }
+            else
+            {
+                TB.AppendText(text);
+            }
+        }
+        void Rest()
+        {
+
+
+            TcpClient tcpcc = new TcpClient("192.168.90.101", 1500);
+
+            do
+            {
+
+                System.Threading.Thread.Sleep(10);
+
+            } while (!tcpcc.Connected);
+
+            WriteToPLC("PC2=RESTART", tcpcc);
+            tcpcc.Close();
+
+            WriteTextSafe("Restart DVR" + Environment.NewLine, textBox1);
+
+        }
+
+
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+
+            textBox2.Text = countdown.ToString();
+            if (countdown-- <= 1)
+            {
+                countdown = countdownC;
+
+                TryConnect();
+            }
+
+
+        }
+
+        public string WriteToPLC(string cmd, TcpClient tcpc) //TCP 讀寫核心
+        {
             try
             {
 
-                TcpClient tcpClient = new TcpClient()
+                byte[] sendBuf = Encoding.ASCII.GetBytes((char)2 + cmd + (char)3);
+                String Str = string.Empty;
 
+
+                tcpc.GetStream().Write(sendBuf, 0, sendBuf.Length);
+                System.Threading.Thread.Sleep(100);
+                if (tcpc.GetStream().DataAvailable == true)
+                {
+
+                    Byte[] rec_buf = new byte[tcpc.Available];
+                    tcpc.GetStream().Read(rec_buf, 0, rec_buf.Length);
+                    Str = Encoding.ASCII.GetString(rec_buf);
+                }
+
+                return Str;
             }
-            catch (Exception)
+            catch (SocketException Sockete)
             {
-
-                throw;
+                return string.Empty;
             }
+
+
+
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            button1.Text = timer2.Enabled.ToString();
+
+
+            timer2.Enabled = true;
         }
     }
 }
